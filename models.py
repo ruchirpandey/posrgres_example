f13f6cee82d2f797dad938aa158363d4f04e05f5
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-03-02 18:48:16 +0530

    asa
    sa

--- models.py
+++ models.py
@@ -19,7 +19,31 @@ class Book(db.Model):
     def serialize(self):
         return {
             'id': self.id, 
-            'name': self.name,
-            'author': self.author,
-            'published':self.published
+            'month': self.month,
+            'date': self.date,
+            'event':self.event
+        }
+
+class Holiday(db.Model):
+    __tablename__ = 'holidays'
+
+    id = db.Column(db.Integer, primary_key=True)
+    month = db.Column(db.String())
+    date = db.Column(db.Integer)
+    event = db.Column(db.String())
+
+    def __init__(self, month, date, event):
+        self.month = month
+        self.date = date
+        self.event = event
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'month': self.month,
+            'date': self.date,
+            'event':self.event
         }
