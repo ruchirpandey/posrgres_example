b8e66d9f6d85d24df0c17e6e7c0b5224c5128acd
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-03-07 22:42:33 +0530

    j
    j

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def get_by_id(month_):
         return jsonify(reply)
         #return jsonify(book.serialize())
     except Exception as e:
-        print "hy"
+        print ("hy")
 	    return(str(e))
 
 @app.route("/add/form",methods=['GET', 'POST'])
