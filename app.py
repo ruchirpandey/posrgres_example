37fccbcb9eac97a8c15d975d0cd8c0145b6ee605
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-03-15 21:49:44 +0530

    w

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def get_by_id():
                     date: {2}
                     event: {3}
                     
-                    """.format(holiday.id, holiday.month, holiday.date, holiday.event)
+                    """.format("Dear student, there is one holiday in"+holiday.month+"on "+holiday.date+"for the occasion of"+holiday.event)
             reply = {"fulfillmentText": response,}
             return jsonify(reply)
         else:
